386f58daee74db3fc96f8aa3f907047d796800b6
Sahil <sahil@asystechs.com>
2025-08-13 23:14:56 +0530

    get food method done

--- RestaurantManagement/controllers/foodController.go
+++ RestaurantManagement/controllers/foodController.go
@@ -1,13 +1,34 @@
 package controllers
 
 import (
+	"context"
+	"net/http"
+	"restaurant-management/database"
+	"restaurant-management/models"
+	"time"
+
 	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
+var foodCollection *mongo.Collection = database.OpenCollection(database.Client, "food")
 
 func GetFood() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		// Logic to get all food items
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		foodId := c.Param("food_id")
+
+		var food models.Food
+
+		err := foodCollection.FindOne(ctx, bson.M{"food_id": foodId}).Decode(&food)
+
+		defer cancel()
+
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Error fetching food item"})
+		}
+		c.JSON(http.StatusOK, gin.H{"food": food})
 	}
 }
 
